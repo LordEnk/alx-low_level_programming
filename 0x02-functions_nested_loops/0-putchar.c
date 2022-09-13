d53d0d662c15506fd80574a016877e85fd992413
Enock Ssesanga <enk.me05@gmail.com>
2022-09-13 13:17:00 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  */
  int main(void)
 {
-	_putchar("_");
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -14,6 +14,7 @@
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
