c3ea18562c5f0083e662c12a6f5c4fc879c5d938
Enock Ssesanga <enk.me05@gmail.com>
2022-09-20 10:14:43 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - SWAps two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a
+
+	*a = *b;
+	*b = c;
+}
