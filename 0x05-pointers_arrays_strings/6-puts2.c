5bae81eea722bd949c0bec75a1defbe11bbce7d3
Enock Ssesanga <enk.me05@gmail.com>
2022-09-20 22:44:32 -0700

    task05

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - main block
+ * @str: string prototype
+ *
+ */
+void puts2(char *str)
+{
+	int i =0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
