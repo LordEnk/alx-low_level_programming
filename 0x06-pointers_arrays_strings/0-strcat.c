46136f127797292919ad93d5785c0f591700c513
Enock Ssesanga <enk.me05@gmail.com>
2022-09-21 11:07:31 -0700

    concatenating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+i#include "main.h"
+
+/**
+ * _strcat - main block
+ * @dest:  string parameter
+ * @src: string parameter 2
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+	char *c;
+
+	while (dest[a] != '\0')
+		a++;
+
+	for (b = 0; src[b] != '\0'; ++b; ++a)
+	{
+		dest[a] = src[b];
+	}
+	dest[a] = '\0';
+	c = dest;
+	return (c);
+}
