fed2826db608ccbb6138d532cfde35a3f50c3758
Enock Ssesanga <enk.me05@gmail.com>
2022-09-21 23:08:07 -0700

    another fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a = 0, b = 0;
+	int a = 0, b;
 
-	while (dest[a] != '\0')
+	while (dest[a])
 		a++;
 
-	for (b = 0; src[b] != '\0'; ++b; ++a)
+	for (b = 0; src[b] != '\0'; ++b)
 	{
 		dest[a] = src[b];
+		a += 1;
 	}
 	dest[a] = '\0';
 	return (dest);
