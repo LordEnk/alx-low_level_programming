3463176c3097a572c669f6bccefc41839398a367
Enock Ssesanga <enk.me05@gmail.com>
2022-09-22 00:00:15 -0700

    lost it

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,7 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
-
-	for (i = 0; i < b; i++)
-	{
-		if (src[i] == '\0')
-		{
-			for (j = i; j < n && dest[j] != '\0'; j++)
-				dest[j] = '\0';
-			break;
-		}
-		dest[i] = src[i];
-	}
+	strncopy(dest, src, n);
 	return (dest);
 }
 
