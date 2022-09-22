622114b665aad7f28efb39c026b03e20092a0960
Enock Ssesanga <enk.me05@gmail.com>
2022-09-21 23:31:57 -0700

    total change

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,15 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat - main block
  * @dest: string parameter
- * @scr: string parameter
+ * @src: string parameter
  * @n: integer
  *Return: pointer
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
-	char *c;
-
-	while (dest[i] != '\0')
-		i++;
-	while (!(src[j] == '\0' || j == n))
-	{
-		dest[i] = scr[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	c = dest
-	return (c);
+	strncat(dest, src, n);
+	return (dest);
 }
