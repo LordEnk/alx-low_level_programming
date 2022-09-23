ce6a199d6f04d66ac7fbd4554562cc2f9441b8d8
Enock Ssesanga <enk.me05@gmail.com>
2022-09-23 07:11:46 -0700

    trial

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,17 +2,18 @@
 
 /**
  * string_toupper - main block
- *
+ * @str: character string to change
  *Return: string
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i = 0;
-	char str[];
 
-	while (str[i] >= 'a' && str[i] <= 'z')
+	while (str[i])
 	{
-		str[i] -= 32
-			i++
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+			i++;
 	}
 	return (str);
+}
