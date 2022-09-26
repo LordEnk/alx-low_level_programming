8c01e726de2ca790cd47d8d8229deb26488477c3
Enock Ssesanga <enk.me05@gmail.com>
2022-09-26 08:22:16 -0700

    fixing strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (*(s + i) == c)
-			return (s + i);
+			return (s[i]);
+		else
+			return (0);
 	}
-	if (*(s + ) == c)
-		return (s + i);
-	return (0);
+}
