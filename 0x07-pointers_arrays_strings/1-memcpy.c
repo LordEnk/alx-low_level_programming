cc04e5351553d96a24ab9bb61f2530ba43982a10
Enock Ssesanga <enk.me05@gmail.com>
2022-09-26 07:40:46 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - main block
+ * @dest: where bytes a copied to
+ * @src: where bytes are copied from
+ * @n: bytes copied
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (unsigned int i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
