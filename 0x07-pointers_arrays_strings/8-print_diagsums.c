b32a3adcbbf5f3fcc9b7206f5292aab523071297
Enock Ssesanga <enk.me05@gmail.com>
2022-09-26 23:10:15 -0700

    diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_diagsums - sum of two diagonals of square matrix
+ * @a: pointer to the matrix
+ * @size: widithof matrix column
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, k, l, m = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + k);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - l - j);
+		m += *(a + k);
+	}
+	printf("%i, %i\n", l, m);
+}
