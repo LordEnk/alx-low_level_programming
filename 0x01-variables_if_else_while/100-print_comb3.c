d5e4948efecd727ac32822fb72854bebbdcc7985
Enock Ssesanga <enk.me05@gmail.com>
2022-12-14 00:41:00 -0800

    corrections to two digits task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-	int a = 48, b = 48;
+	int a = 48, b = 49;
 
-	while (a < 58)
+	while (a < 57)
 	{
-		b = 48;
+		b = 49;
 
 		while (b < 58)
 		{
-			if (a != b && b < a)
+			if (a != b && b > a)
 			{
-				putchar(b);
 				putchar(a);
+				putchar(b);
 
-				if (!(a == 56 && b == 57))
+				if (a != 56 && b != 57)
 				{
 					putchar(',');
 					putchar(' ');
