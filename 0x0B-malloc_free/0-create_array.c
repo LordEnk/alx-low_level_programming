2ee0f21da963cc6f4505403390a88a8ac416a00b
Enock Ssesanga <enk.me05@gmail.com>
2023-01-11 03:06:34 -0800

    malloc and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (0);
-	p = malloc(size * sizeof(char));
+	p = (char*) malloc((size) * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
