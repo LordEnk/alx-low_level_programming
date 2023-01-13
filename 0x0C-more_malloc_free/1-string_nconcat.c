85507876887c51f10581d54b2194d7f5452e827f
Enock Ssesanga <enk.me05@gmail.com>
2023-01-13 01:18:58 -0800

    more malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	if (n > size2)
-	n = size2;
+		n = size2;
+
 	p = malloc((size1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
@@ -45,7 +46,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[i] = s2[i - size1];
 	}
-	p[i] = '\0';
 
-return (p);
+	p[i] = '\0';
+	return (p);
 }
