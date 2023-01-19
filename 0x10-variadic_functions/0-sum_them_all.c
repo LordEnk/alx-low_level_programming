4f96a843ce414a5e3f40e769e97a87907037aa5c
Enock Ssesanga <enk.me05@gmail.com>
2023-01-19 05:31:35 -0800

    variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * sum_them_all - sums its parameters
@@ -11,7 +13,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 	int sum = 0;
-	va_start(ap, n)
+
+	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_strings - prints strings
@@ -15,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	for (i = 0; i < n; i+)
+	for (i = 0; i < n; i++)
 	{
 		strng = va_arg(ap, char*);
 		if (strng == NULL)
