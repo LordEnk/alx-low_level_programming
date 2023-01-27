eebc66f60ec1b65404d35532f418b68cbf8ea7dc
Enock Ssesanga <enk.me05@gmail.com>
2023-01-27 06:36:46 -0800

    singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,12 @@ void before_main(void) __attribute__((constructor));
  * before_main - prints a n output before the main function
  * Return: void
  */
+
 void before_main(void) __attribute__((constructor));
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char *a, *b;
+
+	a = "You're beat! and yet, you must allow,\n";
+	b = "I bore my house upon my back!\n";
+	printf("%s%s", a, b);
 }
