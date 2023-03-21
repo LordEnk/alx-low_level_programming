0a05f5058dfd638ec8967eedab741e2f9114cda8
LordEnk <enk.me05@gmail.com>
2023-03-21 07:37:19 -0700

    c dynamic libraries

---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of a number
+ * @n: The number to operate on
+ * Return: The absolute value of the number
+ */
+
+int _abs(int n)
+{
+  if (n >= 0)
+    return (n);
+  return (-n);
+}
---
+++ 0x18-dynamic_libraries/pyandc.h
@@ -0,0 +1,10 @@
+#ifndef PYANDC_H
+#define PYANDC_H
+
+int add(int, int);
+int sub(int, int);
+int div(int, int);
+int mul(int, int);
+int mod(int, int);
+
+#endif
